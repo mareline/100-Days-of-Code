e7c3becb6525ab6d797f6b64b9cc5df197a6a16f
Mareline <mrami204@fiu.edu>
2022-10-16 23:27:54 -0400

    Updated Code

--- CatVille.py
+++ CatVille.py
@@ -7,6 +7,7 @@ print('''     _                ___       _.--.
        {_.-``-'         {_/ ''')
 
 #Art from https://ascii.co.uk/art/cat
+#Incomplete project
 
 print("Hello! Welcome  to CatVille")
 print("Your mission is to find your flamingo toy. Good luck!")
@@ -22,8 +23,8 @@ if choice1 == "yes":
                 print("Wow, you made a mess in the living room and Mareline took all the toys away. Game over.")
             elif choice4 == "2":
                 print("You went under the bed and found your flamingo toy! Time to play with Mareline :)")
-    else:
-            print("Are you walking on the keyboard again Miso?")
+else:
+    print("Are you walking on the keyboard again Miso?")
 
 
     
